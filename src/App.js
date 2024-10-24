07388d87671e768f3e4e0380a75479b7ab1576c0
leandro-bsf <motorola1710@gmail.com>
2024-10-23 21:23:23 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import jsPDF from 'jspdf';
 import './App.css'
 import logo_whtas from './img/whatsapp.png'
 import logo_insta from './img/instagram.png'
-import userEvent from '@testing-library/user-event';
+import logo from './img/Minha_logo.png'
+
 
 function ImageUploader() {
   const [images, setImages] = useState([]);
@@ -61,15 +62,15 @@ const handleImageLogo = (e) => {
       name: file.name,           // Nome do arquivo
       url: URL.createObjectURL(file), // URL da imagem
     }));
-    
+
     setImages(imageList); // Atualiza o estado com as imagens selecionadas.
   };
   const generatePDF = () => {
     const pageCapa = document.querySelector('.a4-page_capa'); // Seleciona a capa
     const pageContent = document.querySelector('.a4-page'); // Seleciona a página de conteúdo
-  
+
     const pdf = new jsPDF('portrait', 'px', 'a4'); // Cria o PDF no formato A4
-  
+
     // Função auxiliar para adicionar uma página ao PDF
     const addPageToPDF = (page, isLastPage) => {
       return html2canvas(page, { scale: 2 }).then((canvas) => {
@@ -77,15 +78,15 @@ const handleImageLogo = (e) => {
         const imgProps = pdf.getImageProperties(imgData);
         const pdfWidth = pdf.internal.pageSize.getWidth();
         const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-  
+
         pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-        
+
         if (!isLastPage) {
           pdf.addPage(); // Adiciona uma nova página ao PDF
         }
       });
     };
-  
+
     addPageToPDF(pageCapa, false).then(() => {
       addPageToPDF(pageContent, true).then(() => {
         // Usa o valor do input 'dado_aluno' como nome do arquivo PDF
@@ -96,23 +97,23 @@ const handleImageLogo = (e) => {
 
   return (
     <div  className='div_principal'>
-      
+
       <div className="header">
       <div  className='Form'>
          <span>
             Preencha os Dados abaixo para criação  da Capa
          </span> <br/>
-        <input   type="text"   placeholder="Nome Fotografo(a)"  value={nome_fotografo} onChange={(e) => setFotografo(e.target.value)}/>
+        {/* <input   type="text"   placeholder="Nome Fotografo(a)"  value={nome_fotografo} onChange={(e) => setFotografo(e.target.value)}/>
         <input   type="text"   placeholder="Telefone "  value={telefone} onChange={(e) => setTelefone(e.target.value)}/>
-        <input   type="text"   placeholder="Instagran "  value={Aroba_insta} onChange={(e) => setAroba_insta(e.target.value)}/>
+        <input   type="text"   placeholder="Instagran "  value={Aroba_insta} onChange={(e) => setAroba_insta(e.target.value)}/> */}
         <input   type="text"   placeholder="Informação Aluno "  value={dados_aluno} onChange={(e) => setDados_aluno(e.target.value)}/>
-        <input   type="text"   placeholder="texto_capa "  value={texto_capa} onChange={(e) => setTexto_capa(e.target.value)}/>
+        {/* <input   type="text"   placeholder="texto_capa "  value={texto_capa} onChange={(e) => setTexto_capa(e.target.value)}/>
         <br/>
         <span>Selecione sua Logo</span>
         <br/>
-        <input type="file" accept="image/*" onChange={handleImageLogo} />
+        <input type="file" accept="image/*" onChange={handleImageLogo} /> */}
         <br/>
-        <br/>
+        {/* <br/>
          <span> Preencha com a informacao dos Pacotes </span>
          <br/>
         <input   type="text"   placeholder="Pacote1 "  value={Pacote1} onChange={(e) => setPacote1(e.target.value)}/>
@@ -120,100 +121,118 @@ const handleImageLogo = (e) => {
         <input   type="text"   placeholder="Pacote3 "  value={Pacote3} onChange={(e) => setPacote3(e.target.value)}/>
         <input   type="text"   placeholder="Pacote4 "  value={Pacote4} onChange={(e) => setPacote4(e.target.value)}/>
         <br/>
-        <br/>
-        <span>informe a formas pagamento sep. por '-'</span> <br/> 
+        <br/> */}
+        {/* <span>informe a formas pagamento sep. por '-'</span> <br/> */}
         {/* <input   type="text"   placeholder="form_pagamento "  value={form_pagamento} onChange={(e) => setForm_pagamento(e.target.value)}/>
         <input   type="text"   placeholder="text_complementar "  value={text_complementar} onChange={(e) => SetText_complementar(e.target.value)}/> */}
 
         <input   type="text"   placeholder="Data Entrega "  value={data_entrega} onChange={(e) => setData_entrega(e.target.value)}/>
 
-        
-      <br/>
      
-   
+
+
+      <br/>
+
+
       </div>
 
 
       <div  className='Form'>
-         <span>
-            Preencha os Dados abaixo para criação da Grid
-         </span> <br/>
+         {/* <span>  Preencha os Dados abaixo para criação da Grid</span> */}
+          <br/>
         {/* <input   type="text"   placeholder="Texto Complementar"  value={name} onChange={(e) => setName(e.target.value)}/> */}
+         <span> Escola até 35 fotos.</span>
         <input  type="file"  accept="image/*" multiple   onChange={handleImageChange}  />
+
       <br/>
-     
+
       <button onClick={generatePDF}>Gerar PDF</button>
       </div>
 
-     
+
       </div>
       {/* Área onde as imagens e o grid são renderizados */}
       <div className="a4-page_capa">
-         <div  className='nav'> 
-           
+         <div  className='nav'>
+
            <div className='nav_nome_fotografo'>
-           <img  className='logo_cabecalho'  src={imageLogo} />
-           <span>{nome_fotografo} </span>
+           {/* <img  className='logo_cabecalho'  src={imageLogo} /> */}
+            <img  className='logo_cabecalho' src={logo}/>
+           {/* <span>{nome_fotografo} </span> */}
+           <span>DIOVANA LIMA FOTOGRAFIA </span>
            </div>
-        
+
            <div className='nav_tel_insta'>
               <div className='dados'>
                  <img  className= 'logos' src={logo_whtas} alt="Logo"/>
-                 <span>{telefone}</span>  
+                 {/* <span>{telefone}</span> */}
+                  <span>(46)99132-1472  </span>
               </div>
               <div  className='dados'>
                 <img className= 'logos' src={logo_insta} alt="Logo" />
-                  <span>{Aroba_insta}</span>
+                  {/* <span>{Aroba_insta}</span> */}
+                  <span>@diovanna_lima  </span>
+
               </div>
-             
+
           </div>
          </div>
 
 
           <div nav_meio>
              <div className='nav_meio_aluno'>
-             
+
                 <span>{dados_aluno}</span>
              </div>
              <div className='nav_meio_texto'>
              <span style={{color:'red'}}>Senhores pais ou responsáveis, o ensaio fotográfico ficou lindo!</span>
-             <h3> {texto_capa} </h3>
-            
-           
-             <span> NOME DO RESPONSAVEL:______________________</span> <br/>
-             <span> WHATSAPP DO RESPONSAVEL:____________________</span>
+             {/* <h3> {texto_capa} </h3> */}
+             <h3>Aproveite e também garanta essa linda recordação, escolha as
+             fotos com carinho, seu filho(a) merece!</h3>
+
+
+             <span className='dados_pais'> NOME DO RESPONSAVEL:_________________________________</span> <br/>
+             <span className='dados_pais'> WHATSAPP DO RESPONSAVEL:_______________________________</span>
 
              </div>
-            
+
 
           </div>
-           <div className='div_Pacotes'> 
+           <div className='div_Pacotes'>
              <div className='nav_meio_aluno' >
                 <span>MARQUE O PACOTE DESEJADO :</span>
 
              </div>
-           
+
              <div className='pacote'>
              <div className="teck_pacote"></div>
-             <span className='text_pacote'>{Pacote1}</span>
-             </div>
+             {/* <span className='text_pacote'>{Pacote1}</span> */}
+              <span className='text_pacote'>PACOTE 1: R$50,00 (1 fotão 20x25 + 1 foto menor 10x15
+                + brinde as 2 fotos em arquivo digital direto no seu WhatsApp).</span>
+            </div>
       	      <br/>
              <div className='pacote'>
              <div className="teck_pacote"></div>
-             <span className='text_pacote'>{Pacote2}</span>
+             {/* <span className='text_pacote'>{Pacote2}</span> */}
+             <span className='text_pacote'>PACOTE 2: R$80,00 (1 fotão 20x25 + 4 fotos menores 10x15
+              + brinde as 4 fotos em arquivo digital direto no seu WhatsApp).</span>
              </div>
              <br/>
 
              <div className='pacote'>
              <div className="teck_pacote"></div>
-             <span className='text_pacote'>{Pacote3}</span>
+             {/* <span className='text_pacote'>{Pacote3}</span> */}
+             <span className='text_pacote'>PACOTE 3: R$120,00 (1 quadro com foto 20x30 + 5 fotos menores 10x15
+              + brinde as 5 fotos em arquivo digital direto no seu WhatsApp).</span>
              </div>
-             <br/>  
+             <br/>
              <div className='pacote'>
              <div className="teck_pacote"></div>
-             <span className='text_pacote'>{Pacote4}</span>
+             {/* <span className='text_pacote'>{Pacote4}</span> */}
+             <span className='text_pacote'>PACOTE 4: R$160,00 (1 quadro com foto 20x30 + 10 fotos menores
+              10x15 + brinde as 10 fotos em arquivo digital direto no seu WhatsApp).</span>
              </div>
-             
+
 
 
            </div>
@@ -223,25 +242,26 @@ const handleImageLogo = (e) => {
               <div className='Div_pagamento_text'>
               <h2>PIX - DINHEIRO - CARTAO DE DÉBITO - CARTAO DE CRÉDITO</h2>
               {/* <h2>{form_pagamento}</h2> */}
-              <h2 style={{ color: 'red'}}>Observação:  PACOTES 3 E 4 , PARCELAMOS NO CARTAO DE CRÉDITO EM   ATÉ 4X SEM JUROS 
+              <br/>
+              <h2 style={{ color: 'red'}}>Observação:  PACOTES 3 E 4 , PARCELAMOS NO CARTAO DE CRÉDITO EM   ATÉ 4X SEM JUROS
               </h2>
               {/* <h2>{text_complementar}</h2> */}
               </div>
-         
+
 
            </div>
            <div className='roda_pes'>
              <h2>PAGAMENTO E ENTREGA DAS FOTOS DIA {data_entrega}</h2>
            </div>
       </div>
-  
+
       <div className="a4-page">
         <div className='grid_titulo'>
         <span>{dados_aluno}</span>
         <h3>MARQUE  COM X AS FOTOS ESCOLHIDAS</h3>
         {/* {name &&  <h2> {name}</h2>} */}
         </div>
-     
+
         {images.length > 0 ? (
           <div className="image-grid">
             {images.map((image, index) => (
